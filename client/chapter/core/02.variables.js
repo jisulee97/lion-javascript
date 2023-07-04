27c133713c0c4786a0a3bac74c304123dfe8198b
leejisu <ejeesoo97@gmail.com>
2023-07-04 11:10:49 +0900

    변수명 짓기

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,42 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin, name;
+name = 'John';
+admin = name;
+// console.log(admin);
+
+let ourPlanetName = 'Earth';
+let currentOnlineUserName = 'tiger';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365;
+
+// - 구매 결제 여부(했어 안 했어(is) / 있어 없어(has))
+let isPayment = false;
+let hasClassName = true;
+
+// - 구매 결제 내역
+let paymentHistory;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'NIKE';
+
+// - 오늘의 운세
+let fortuneOfToday;
+
+// - 상품 정보
+const productInfomation = '';
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
--- client/main.js
+++ client/main.js
@@ -1,9 +1,3 @@
-
-
-
-console.log('hello js!');
-
 const a = 10;
 
 const b = 10;
-
