baecbd7a537f59348b799497ffe3c89c70693ee6
leejisu <ejeesoo97@gmail.com>
2023-07-17 15:33:16 +0900

    getNodes 유틸 함수 생성

--- client/lib/dom/getNode.js
+++ client/lib/dom/getNode.js
@@ -4,3 +4,12 @@ function getNode(node) {
   }
   return document.querySelector(node);
 }
+
+//# 여러 요소를 잡을 경우
+
+function getNodes(node) {
+  if (typeof node !== 'string') {
+    throw new Error('getNode 함수의 인수는 문자 타입이어야 합니다.');
+  }
+  return document.querySelectorAll(node);
+}
