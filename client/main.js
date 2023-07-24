015be7ece3bb6f82e0bbeff56a35f30ddd7070e0
leejisu <ejeesoo97@gmail.com>
2023-07-24 14:32:39 +0900

    주사위가 멈추면 기록/초기화 버튼 활성화가 되는 코드 구현

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { diceAnimation, getNode, getNodes } from './lib/index.js';
 
-//# [phase-1] 주사위 굴리기
+//@ [phase-1] 주사위 굴리기
 //# 1. dice animation 불러오기
 //# 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation 실행될 수 있도록
 //#           - 주사위 굴리기 버튼을 가져온다
@@ -44,15 +44,46 @@ const [startButton, recondButton, resetButton] = getNodes(
 
 //# 4. 즉시실행함수로 변경(클로저 + IIFE 를 사용한 변수 보호)
 
+// const handleRollingDice = ((e) => {
+//   let isClicked = false;
+//   let stopAnimation;
+
+//   return () => {
+//     if (!isClicked) {
+//       stopAnimation = setInterval(diceAnimation, 100);
+//     } else {
+//       clearInterval(stopAnimation);
+//     }
+
+//     isClicked = !isClicked;
+//   };
+// })();
+
+// startButton.addEventListener('click', handleRollingDice);
+
+//@ [phase-2] 레코드 리스트 control / view
+
+//# 1. 주사위가 멈추면 기록/초기화 버튼 활성화
+
+// 배열 구조 분해 할당
+
+//# 과제: disableElement(node)/ enableElement(node) 함수 만들어보기
+
 const handleRollingDice = ((e) => {
   let isClicked = false;
   let stopAnimation;
 
   return () => {
     if (!isClicked) {
+      // 주사위 play
       stopAnimation = setInterval(diceAnimation, 100);
+      recondButton.disabled = true;
+      resetButton.disabled = true;
     } else {
+      // 주사위 stop
       clearInterval(stopAnimation);
+      recondButton.disabled = false;
+      resetButton.disabled = false;
     }
 
     isClicked = !isClicked;
